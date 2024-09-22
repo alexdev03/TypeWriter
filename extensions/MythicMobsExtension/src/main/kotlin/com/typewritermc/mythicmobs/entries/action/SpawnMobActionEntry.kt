85c7d4e1719699b489e09f28007740cb29e8f2da
AlexDev_ <56083016+alexdev03@users.noreply.github.com>
2024-09-22 14:41:30 +0200

    Some fixes

--- extensions/MythicMobsExtension/src/main/kotlin/com/typewritermc/mythicmobs/entries/action/SpawnMobActionEntry.kt
+++ extensions/MythicMobsExtension/src/main/kotlin/com/typewritermc/mythicmobs/entries/action/SpawnMobActionEntry.kt
@@ -10,18 +10,16 @@ import com.typewritermc.engine.paper.entry.Criteria
 import com.typewritermc.engine.paper.entry.Modifier
 import com.typewritermc.engine.paper.entry.TriggerableEntry
 import com.typewritermc.engine.paper.entry.entries.ActionEntry
-import com.typewritermc.engine.paper.entry.entries.ConstVar
-import com.typewritermc.engine.paper.entry.entries.Var
-import com.typewritermc.engine.paper.entry.entries.get
 import com.typewritermc.engine.paper.extensions.placeholderapi.parsePlaceholders
 import com.typewritermc.engine.paper.plugin
 import com.typewritermc.engine.paper.utils.ThreadType.SYNC
 import com.typewritermc.engine.paper.utils.toBukkitLocation
-import io.github.retrooper.packetevents.util.SpigotConversionUtil.toBukkitLocation
+import com.typewritermc.engine.paper.utils.toBukkitPlayerLocation
 import io.lumine.mythic.api.mobs.entities.SpawnReason
 import io.lumine.mythic.bukkit.BukkitAdapter
 import io.lumine.mythic.bukkit.MythicBukkit
-import io.lumine.mythic.core.skills.placeholders.PlaceholderExecutor.parsePlaceholders
+import io.lumine.mythic.core.skills.variables.Variable
+import io.lumine.mythic.core.skills.variables.VariableType
 import org.bukkit.entity.Player
 
 
@@ -43,6 +41,9 @@ class SpawnMobActionEntry(
     private val mobName: Var<String> = ConstVar(""),
     private val level: Var<Double> = ConstVar(1.0),
     private val onlyVisibleForPlayer: Boolean = false,
+    @Placeholder
+    @Help("The variables to set for the mob. Format: variableName:type=value. Example: test:INTEGER=100")
+    private val variables : List<String> = emptyList(),
     @WithRotation
     private var spawnLocation: Var<Position> = ConstVar(Position.ORIGIN),
 ) : ActionEntry {
@@ -53,12 +54,35 @@ class SpawnMobActionEntry(
         if (!mob.isPresent) return
 
         SYNC.launch {
-            mob.get().spawn(BukkitAdapter.adapt(spawnLocation.get(player).toBukkitLocation()), level.get(player), SpawnReason.OTHER) {
+            val activeMob = mob.get().spawn(BukkitAdapter.adapt(spawnLocation.get(player).toBukkitLocation()), level.get(player), SpawnReason.OTHER) {
                 if (onlyVisibleForPlayer) {
                     it.isVisibleByDefault = false
                     player.showEntity(plugin, it)
                 }
+
             }
+
+            val variableMap = mutableMapOf<String, Any>()
+            val variableType = mutableMapOf<String, VariableType>()
+            variables.forEach { variable ->
+                val split = variable.split("=")
+                if (split.size == 2) {
+                    val split2 = split[0].split(":")
+                    if (split2.size == 2) {
+                        val type = VariableType.valueOf(split2[1].uppercase())
+                        val name = split2[0]
+                        variableMap[name] = split[1].parsePlaceholders(player)
+                        variableType[name] = type
+                    }
+                }
+            }
+
+            variableMap.forEach { (key, value) ->
+                val variable = Variable.ofType(variableType[key]!!, value)
+                activeMob.variables.put(key, variable)
+            }
+
         }
+
     }
 }
