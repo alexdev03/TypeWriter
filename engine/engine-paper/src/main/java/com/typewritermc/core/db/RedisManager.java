2630c9a0d32a52958aeb5bf4292f9002d07a9ca3
AlexDev_ <56083016+alexdev03@users.noreply.github.com>
2024-11-14 17:03:24 +0100

    Improved database system

--- engine/engine-paper/src/main/java/com/typewritermc/core/db/RedisManager.java
+++ engine/engine-paper/src/main/java/com/typewritermc/core/db/RedisManager.java
@@ -5,7 +5,9 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.typewritermc.engine.paper.db.RedisAbstract;
 import com.typewritermc.engine.paper.db.RedisProxyMap;
+import com.typewritermc.engine.paper.entry.entries.GroupId;
 import com.typewritermc.engine.paper.facts.FactData;
+import com.typewritermc.engine.paper.facts.FactDatabase;
 import com.typewritermc.engine.paper.facts.FactId;
 import io.lettuce.core.RedisClient;
 import org.jetbrains.annotations.NotNull;
@@ -18,15 +20,18 @@ import java.time.ZoneOffset;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.stream.Collectors;
 
 public class RedisManager extends RedisAbstract {
 
     private final Gson gson;
     private final RedisProxyMap redisProxyMap;
+    private final FactDatabase factDatabase;
     private final ZoneOffset zoneOffset = OffsetDateTime.now().getOffset();
 
-    public RedisManager(RedisProxyMap redisProxyMap, RedisClient lettuceRedisClient, int poolSize) {
+    public RedisManager(FactDatabase factDatabase, RedisProxyMap redisProxyMap, RedisClient lettuceRedisClient, int poolSize) {
         super(lettuceRedisClient, poolSize);
+        this.factDatabase = factDatabase;
         gson = new Gson();
         this.redisProxyMap = redisProxyMap;
         this.registerSub(new String[]{RedisKeys.FACTS_UPDATE.getKey()});
@@ -59,17 +64,51 @@ public class RedisManager extends RedisAbstract {
                 .thenApply(this::deserializeFactMap));
     }
 
+    public CompletionStage<Map<FactId, FactData>> loadPlayerFacts(UUID uuid) {
+        return getConnectionAsync(c -> c.hgetall(RedisKeys.PLAYER_FACTS.getKey() + uuid.toString()))
+                .thenApply(map -> deserializePlayerFactMap(uuid, map));
+    }
+
     public void saveFacts(Map<FactId, FactData> facts) {
         String factDataAsString = serializeFactMap(facts);
         getConnectionAsync(c -> c.hset(RedisKeys.FACS.getKey(), facts.toString(), factDataAsString));
     }
 
     public void saveFact(FactId factId, FactData factData) {
-//        System.out.println("Pre send update: " + factId + " " + factData);
+        final UUID targetUUID = factDatabase.readUUID(factId);
+        if (targetUUID == null) {
+            System.out.println("Could not find target UUID for " + factId);
+            return;
+        }
+
+        if(factDatabase.getPlayerUUIDs().contains(targetUUID)) {
+            savePlayerFact(targetUUID, factId, factData);
+        } else {
+            saveGroupFact(factId, factData);
+            sendUpdate(factId, factData);
+        }
+
+////        System.out.println("Pre send update: " + factId + " " + factData);
+//        String factIdAsString = gson.toJson(factId);
+//        String factDataAsString = serializeFactData(factData);
+//        getConnectionAsync(c -> c.hset(RedisKeys.FACS.getKey(), factIdAsString, factDataAsString));
+    }
+
+    public CompletionStage<Map<FactId, FactData>> loadGroupFacts() {
+        return getConnectionAsync(c -> c.hgetall(RedisKeys.GROUP_FACTS.getKey()))
+                .thenApply(this::deserializeFactMap);
+    }
+
+    private void savePlayerFact(UUID uuid, FactId factId, FactData factData) {
+        String factIdAsString = factId.getEntryId();
+        String factDataAsString = serializeFactData(factData);
+        getConnectionAsync(c -> c.hset(RedisKeys.PLAYER_FACTS.getKey() + uuid.toString(), factIdAsString, factDataAsString));
+    }
+
+    private void saveGroupFact(FactId factId, FactData factData) {
         String factIdAsString = gson.toJson(factId);
         String factDataAsString = serializeFactData(factData);
-        getConnectionAsync(c -> c.hset(RedisKeys.FACS.getKey(), factIdAsString, factDataAsString));
-        sendUpdate(factId, factData);
+        getConnectionAsync(c -> c.hset(RedisKeys.PLAYER_FACTS.getKey(), factIdAsString, factDataAsString));
     }
 
     public CompletionStage<FactData> loadFact(FactId factId) {
@@ -95,6 +134,15 @@ public class RedisManager extends RedisAbstract {
         return new File(System.getProperty("user.dir")).getName();
     }
 
+    public void savePlayerUUID(UUID uuid) {
+        getConnectionAsync(c -> c.sadd(RedisKeys.PLAYER_UUIDS.getKey(), uuid.toString()));
+    }
+
+    public CompletionStage<Set<UUID>> loadPlayerUUIDs() {
+        return getConnectionAsync(c -> c.smembers(RedisKeys.PLAYER_UUIDS.getKey()))
+                .thenApply(set -> set.stream().map(UUID::fromString).collect(Collectors.toSet()));
+    }
+
     private Map<FactId, FactData> deserializeFactMap(String json) {
         final JsonArray jsonArray = gson.fromJson(json, JsonArray.class);
         final Map<FactId, FactData> map = new HashMap<>();
@@ -111,6 +159,16 @@ public class RedisManager extends RedisAbstract {
         final Map<FactId, FactData> factMap = new HashMap<>();
         map.forEach((key, value) -> {
             final FactId factId = gson.fromJson(key, FactId.class);
+            final FactData factData = deserializeFactData(value);
+            factMap.put(factId, factData);
+        });
+        return factMap;
+    }
+
+    private Map<FactId, FactData> deserializePlayerFactMap(UUID player, Map<String, String> map) {
+        final Map<FactId, FactData> factMap = new HashMap<>();
+        map.forEach((key, value) -> {
+            final FactId factId = factDatabase.createFactId(key, player);
             final FactData factData = deserializeFactData(value);
             factMap.put(factId, factData);
         });
@@ -195,7 +253,10 @@ public class RedisManager extends RedisAbstract {
 
     private enum RedisKeys {
         FACS("TW_FACTS"),
+        PLAYER_FACTS("typewriter:facts:player:"),
+        GROUP_FACTS("typewriter:facts:group:"),
         FACTS_UPDATE("TW_FACTS_UPDATE"),
+        PLAYER_UUIDS("typewriter:facts:player:uuids"),
         USERNAMES("TW_USERNAMES");
 
         private final String key;
