9a8746a56bdc8c5b28049b9bb45d47afda554153
AlexDev_ <56083016+alexdev03@users.noreply.github.com>
2024-12-15 21:35:02 +0100

    Fixed mm despawn entry

--- extensions/MythicMobsExtension/src/main/kotlin/com/typewritermc/mythicmobs/entries/action/DespawnMobsActionEntry.kt
+++ extensions/MythicMobsExtension/src/main/kotlin/com/typewritermc/mythicmobs/entries/action/DespawnMobsActionEntry.kt
@@ -2,6 +2,7 @@ package com.typewritermc.mythicmobs.entries.action
 
 import com.typewritermc.core.books.pages.Colors
 import com.typewritermc.core.entries.Ref
+import com.typewritermc.core.extension.annotations.Default
 import com.typewritermc.core.extension.annotations.Entry
 import com.typewritermc.core.extension.annotations.Help
 import com.typewritermc.core.extension.annotations.Placeholder
@@ -20,7 +21,12 @@ import io.lumine.mythic.core.skills.placeholders.PlaceholderExecutor.parsePlaceh
 import org.bukkit.entity.Player
 
 
-@Entry("despawn_mythicmobs_mobs", "Despawn MythicMobs mobs from variable", Colors.ORANGE_RED, "fluent:crown-subtract-24-filled")
+@Entry(
+    "despawn_mythicmobs_mobs",
+    "Despawn MythicMobs mobs from variable",
+    Colors.ORANGE_RED,
+    "fluent:crown-subtract-24-filled"
+)
 
 class DespawnMobsActionEntry(
     override val id: String = "",
@@ -30,14 +36,19 @@ class DespawnMobsActionEntry(
     override val triggers: List<Ref<TriggerableEntry>> = emptyList(),
     @Help("The variables to set for the mob. Format: variableName=value. Example: test=100")
     @Placeholder
-    private val variables : List<String> = emptyList(),
+    private val variables: List<String> = emptyList(),
 ) : ActionEntry {
     override fun execute(player: Player) {
         super.execute(player)
         //map variables in a map, the key is the variable name, the value is the value parsed with papi
         val variablesMap = variables.associate { it.split("=")[0] to it.split("=")[1].parsePlaceholders(player) }
 
-        val mobs = MythicBukkit.inst().mobManager.activeMobs.filter { it.variables.asMap().entries.all { entry -> variablesMap.containsKey(entry.key) && entry.value.toString() == variablesMap[entry.key]!!.toString() } }
+        val mobs = MythicBukkit.inst().mobManager.activeMobs.filter {
+            variablesMap.entries.all { entry ->
+                it.variables.has(entry.key)
+                        && entry.value == it.variables.get(entry.key).toString()
+            }
+        }
 
         SYNC.launch {
             mobs.forEach {
